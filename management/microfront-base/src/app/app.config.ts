a048f12f9eb18dc8114612bf9262197296477185
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-02-28 23:39:21 -0300

    instalndo e configurando i18n no host

--- management/microfront-base/src/app/app.config.ts
+++ management/microfront-base/src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
+
+const SUPPORTED_LANGS = ['pt', 'en', 'es'];
+const DEFAULT_LANG = 'en';~
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({ defaultLanguage: DEFAULT_LANG }),
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: '.json',
+    }),
+  ],
 };
--- management/microfront-base/src/app/home/home.ts
+++ management/microfront-base/src/app/home/home.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+  private translate = inject(TranslateService);
+
+  ngOnInit(): void {
+    this.translate.use('pt');
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
+}
