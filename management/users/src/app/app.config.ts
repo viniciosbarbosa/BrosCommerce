d517cb04c2b03e7ca07e3bdf2da6af090c00af62
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-03-01 22:46:50 -0300

    reformulando i18n

--- management/users/src/app/app.config.ts
+++ management/users/src/app/app.config.ts
@@ -1,22 +1,11 @@
 import {
   ApplicationConfig,
-  inject,
-  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideTranslateService, TranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
-import { environment } from '../environments/environment';
-
-function getBrowserLang(): string {
-  const browserLang = navigator.language?.split('-')[0];
-
-  return environment.supportedLangs.includes(browserLang) ? browserLang : environment.defaultLang;
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,16 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideTranslateService({ fallbackLang: environment.defaultLang }),
-    provideTranslateHttpLoader({
-      prefix: './assets/i18n/',
-      suffix: '.json',
-    }),
-    provideAppInitializer(() => {
-      const translate = inject(TranslateService);
-      const lang = getBrowserLang();
-      translate.addLangs(environment.supportedLangs);
-      return translate.use(lang);
-    }),
   ],
 };
--- management/users/src/app/features/management/management.ts
+++ management/users/src/app/features/management/management.ts
@@ -1,15 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-management',
+  standalone: true,
   imports: [TranslateModule],
   templateUrl: './management.html',
   styleUrl: './management.scss',
 })
-export class Management {
+export class Management implements OnInit {
+  private http = inject(HttpClient);
   private translate = inject(TranslateService);
 
-  changeLanguage(lang: string) {
-    this.translate.use(lang);
+  ngOnInit(): void {
+    const langs = ['pt', 'en', 'es'];
+    langs.forEach((lang) => {
+      this.http.get(`http://localhost:4203/assets/i18n/${lang}.json`).subscribe((translations) => {
+        this.translate.setTranslation(lang, translations as any, true);
+      });
+    });
   }
 }
